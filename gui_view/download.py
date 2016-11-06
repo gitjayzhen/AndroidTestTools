3739a558b568cd7b541576e0dd579295b50bd481
jayzhen <jayzhen_testing@163.com>
2016-11-06 22:13:47 +0800

    删除url地址

--- gui_view/download.py
+++ gui_view/download.py
@@ -2,7 +2,7 @@
 from gui_controller.downloadApk import DownloadApk
 import time
 downobj = DownloadApk()
-forgeturl = 'http://10.10.152.200/youku/android/V5.8/'
+forgeturl = ''
 urls = downobj.get_apk_link_urls(forgeturl)
 url = forgeturl+urls[0]
 print "url >>> ",url
